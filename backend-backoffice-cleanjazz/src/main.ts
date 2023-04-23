91ef0ef0bf817c62587b0c4172f44259ef908dfb
cmmz22173 <69021120+cmmz22173@users.noreply.github.com>
2023-04-23 13:49:49 -0600

    enable Cors para el API en render

--- backend-backoffice-cleanjazz/src/main.ts
+++ backend-backoffice-cleanjazz/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001', // Reemplace con el origen de su aplicación cliente
+    origin: '*', // Reemplace con el origen de su aplicación cliente
   });
   
   await app.listen(3000);
